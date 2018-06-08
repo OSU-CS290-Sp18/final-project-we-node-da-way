4620832082fa19ae1842aa1ad5481617bf83f86c
Alex Liechty <liechtya@oregonstate.edu>
2018-06-08 11:10:14 -0700

    Added some MONGO stuff

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ var path = require('path');
 var express = require('express');
 var express_handlebars = require('express-handlebars');
 var handlebars = require('handlebars');
+var mongoClient = require('mongodb').MongoClient;
 
 var testData = require('./testData');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
 var options = {
-	
+
 	index: false
-	
+
 };
 
 app.use(express.static('public', options));
@@ -22,35 +31,35 @@ app.set('view engine', 'handlebars');
 
 
 app.get('/', function(req, res, next) {
-	
+
 	res.status(200);
 	res.render('index', {
-		
+
 		products: testData
-		
+
 	});
-	
+
 });
 
 app.get('/index', function(req, res, next) {
-	
+
 	res.status(200);
 	res.render('index', {
-		
+
 		products: testData
-		
+
 	});
-	
+
 });
 
 app.get('*', function(req, res, next) {
-	
+
 	res.status(404);
 	res.render('404');
 })
 
 app.listen(port, function() {
-	
+
 	console.log("This memeingful server is running on port", port);
-	
+
 });
